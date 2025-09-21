87a8db5cb8c97bfca3826cb83d4c36786bcb5b0f
jonathan <jonathhu@stud.ntnu.no>
2025-09-21 11:46:27 +0200

    Remove unnecessary comments

--- plib_tcc0.c
+++ plib_tcc0.c
@@ -1,56 +1,3 @@
-/*******************************************************************************
-  TCC Peripheral Library Interface Source File
-
-  Company
-    Microchip Technology Inc.
-
-  File Name
-    plib_tcc0.c
-
-  Summary
-    TCC0 peripheral library source file.
-
-  Description
-    This file implements the interface to the TCC peripheral library.  This
-    library provides access to and control of the associated peripheral
-    instance.
-
-*******************************************************************************/
-
-// DOM-IGNORE-BEGIN
-/*******************************************************************************
-* Copyright (C) 2018 Microchip Technology Inc. and its subsidiaries.
-*
-* Subject to your compliance with these terms, you may use Microchip software
-* and any derivatives exclusively with Microchip products. It is your
-* responsibility to comply with third party license terms applicable to your
-* use of third party software (including open source software) that may
-* accompany Microchip software.
-*
-* THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER
-* EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED
-* WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A
-* PARTICULAR PURPOSE.
-*
-* IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
-* INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND
-* WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS
-* BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO THE
-* FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN
-* ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
-* THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
-*******************************************************************************/
-// DOM-IGNORE-END
-
-
-// *****************************************************************************
-// *****************************************************************************
-// Section: Included Files
-// *****************************************************************************
-// *****************************************************************************
-
-/*  This section lists the other files that are included in this file.
-*/
 #include "interrupts.h"
 #include "plib_tcc0.h"
 
@@ -236,12 +183,3 @@ void __attribute__((used)) TCC0_OTHER_InterruptHandler(void)
     }
 
 }
-
-
-     
-
-
-
-/**
- End of File
-*/
